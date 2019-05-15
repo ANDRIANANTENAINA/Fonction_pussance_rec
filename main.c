2e06c6322ac1dfac04b0916a08091e587e900b91
ANDRIANANTENAINA <antnfaly@gmail.com>
2019-05-15 14:54:20 +0100

    Creation du fonction puissance recursive

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double puissance(double x, double n);
+
+int main()
+{
+    double i; int x,n;
+
+    printf("Hello world! \n Puissance de la forme x ^ n \n");
+    printf("Entre x: ");scanf("%d",&x);
+    printf("Entre n: ");scanf("%d",&n);
+
+    i=puissance(x,n);
+    printf("\n\tResutat de %d ^ %d = %f \n\n",x,n,i);
+    return 0;
+}
+
+double puissance(double x, double n){
+    if(n == 0) return 1;
+    return (puissance(x,(n-1)) * x);
+}
